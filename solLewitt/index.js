294b55388bc518dd0176b2583031fdb32e5b8f63
dom <adventuresenglish.info@gmail.com>
2023-12-04 21:54:17 -0500

    fuck around and find out

--- solLewitt/index.js
+++ solLewitt/index.js
@@ -1,125 +1,132 @@
-import {
-    select,
-    range,
-    symbol,
-    symbols,
-  } from 'd3';
-  
+
+
+  const numOfBlocks = d3.symbols.length;
   const width = window.innerWidth;
-  const height = window.innerHeight;
-  
-	const randomColor = () => `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+	const height = window.innerHeight;
+  const side = width / numOfBlocks;
+  const halfSide = side / 2;
+  const randomNum = () => Math.floor(Math.random() * 256);
+	const numOfRows = Math.floor(height / side);
+  const randomColor = () =>
+    `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
   function isEven(n) {
     return /^-?\d*[02468]$/.test(n);
   }
-  
-  const svg = select('body')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
-  
- const svgFn = function(num, normal) {
-  	const n = 100;
-    const k = width/symbols.length/20;
-		return svg
-      .append('g')
-      .selectAll('rect')
-      .data(range(n))
-      .join('rect')
-      .attr('y', (d) => d * 10)
-      .attr('x', width/7*num)
-      .attr('width', width/7)
-      .attr('height', 10)
-      .attr('mask', normal ? 'url(#mask-1)' : 'url(#mask-2)')
-    	.attr('fill', randomColor),
-      
-      svg
-      .append('g')
-      .selectAll('rect')
-      .data(range(n))
-      .join('rect')
-      .attr('y', (d) => d * 10)
-      .attr('x', width/7*num)
-      .attr('width', width/7)
-      .attr('height', 10)
-      .attr('mask', normal ? 'url(#mask-1)' : 'url(#mask-2)')
-    	.attr('fill', randomColor),
-  
-		svg
-      .append('g')
-      .selectAll('rect')
-      .data(range(k))
-      .join('rect')
-      .attr('x', (d) => d * 70 + width/7*num)
-      .attr('width', 70)
-      .attr('height', height)
-      .attr('mask', normal ? 'url(#mask-2)' : 'url(#mask-1)')
-  		.attr('fill', randomColor),
-      
-    svg
-      .append('g')
-      .selectAll('rect')
-      .data(range(k))
-      .join('rect')
-      .attr('x', (d) => d * 10 + width/7*num + width/14)
-      .attr('width', 10)
-      .attr('height', height)
-      .attr('mask', normal ? 'url(#mask-1)' : 'url(#mask-2)')
-  		.attr('fill', randomColor)
-  }
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  	.attr(
+            "transform",
+              `translate(0, ${height/2 -halfSide})`
+          )
+  const subSvgFn = function (selection, num, normal) {
+    const q = 10 + Math.random() * 20;
+    const k =  Math.random() * 8;
+    const slatWidth = Math.random() * 14 + 3;
+    const slot = side*num;
 
-const render = function () {
-	for (let i = 0; i < symbols.length; i++) {
-   svgFn(i, isEven(i))
-  }
-}
+      
+      const sel1 = selection
+        .append("g")
+        .selectAll("rect")
+        .data(d3.range(q))
+        .join("rect")
+        .attr("y", (d) => d * slatWidth)
+        .attr("x", slot + halfSide)
+        .attr("width", halfSide)
+        .attr("height", slatWidth)
+        .attr("mask", normal ? "url(#mask-1)" : "url(#mask-2)")
+        .attr("fill", randomColor);
+      
+      const sel2 =selection
+        .append("g")
+        .selectAll("rect")
+        .data(d3.range(q))
+        .join("rect")
+        .attr("y", (d) => d * slatWidth)
+        .attr("x", slot)
+        .attr("width", halfSide)
+        .attr("height", slatWidth)
+        .attr("mask", normal ? "url(#mask-1)" : "url(#mask-2)")
+        .attr("fill", randomColor);
+ 
 
-  const renderMask = function (
-    selection,
-    id,
-    inverted
-  ) {
-    const mask = selection
-      .append('mask')
-      .attr('id', id);
-  	  
-    mask
-      .append('rect')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('fill', inverted ? 'black' : 'white');
-    	
+     const sel3 = selection
+        .append("g")
+        .selectAll("rect")
+        .data(d3.range(k))
+        .join("rect")
+        .attr("x", (d) => d * halfSide + slot)
+        .attr("width", halfSide)
+        .attr("height", side)
+        .attr("mask", normal ? "url(#mask-2)" : "url(#mask-1)")
+        .attr("fill", randomColor);
+
+      const sel4 = selection
+        .append("g")
+        .selectAll("rect")
+        .data(d3.range(k))
+        .join("rect")
+        .attr("x", (d) => d * slatWidth + slot + halfSide)
+        .attr("width", slatWidth)
+        .attr("height", side)
+        .attr("mask", normal ? "url(#mask-1)" : "url(#mask-2)")
+        .attr("fill", randomColor)
+
   
+  };
+
+  const renderMask = function (selection, id, inverted, arr) {
+    const mask = selection.append("mask").attr("id", id);
+
     mask
-      .selectAll('g')
-      .data(range(symbols.length))
+      .append("rect")
+      .attr("width", width)
+      .attr("height", side)
+      .attr("fill", inverted ? "black" : "white");
+
+    mask
+      .selectAll("g")
+      .data(d3.range(numOfBlocks))
       .join((enter) =>
         enter
-          .append('g')
+          .append("g")
           .attr(
-            'transform',
+            "transform",
             (d) =>
               `translate(${
-                (d * width) / symbols.length + width / (symbols.length*2)
-              }, ${height / 2})`
+                (d * width) / numOfBlocks + width / (numOfBlocks * 2)
+              }, ${halfSide})`
           )
-          .append('path')
-          .attr('d', (d) =>
-            symbol(symbols[d], 5000)()
-          )
-          .attr(
-            'fill',
-            inverted ? 'white' : 'black'
-          )
+          .append("path")
+          .attr("d", (d) => d3.symbol(d3.symbols[arr[d]], 5000)())
+          .attr("fill", inverted ? "white" : "black")
       );
   };
-  
 
-	render()
-  svg
-    .call(renderMask, 'mask-1', false)
-    .call(renderMask, 'mask-2', true);
+  const renderBlender = () => {
+     
+    const maskArr = [];
+  for (let i = 0; i < numOfBlocks; i++) {
+    const el = Math.floor(Math.random() * numOfBlocks);
+    maskArr.push(el);
+  }
 
-    setTimeout(function () { 
-      location.reload();
-    }, 3 * 1000);
+      for (let i = 0; i < numOfBlocks; i++) {
+        subSvgFn(svg, i, isEven(i));
+      }
+      svg.call(renderMask, "mask-1" , false, maskArr).call(renderMask, "mask-2", true, maskArr)
+    //}
+  };
+  renderBlender();
+
+  setTimeout(function () {
+    location.reload();
+  }, 2800);
+
+
+
+
+
